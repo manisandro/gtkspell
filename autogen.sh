manisandro/gtkspell

commit adeb8ab65f2d98efc0af05fbb7afa88540640b92

Let autogen.sh be error aware
Currently the script won't end prematurely when an error is occurred,
this patch enables the errexit shell option to make it bail out once
an error is occurred.

Refer-to: DESCRIPTION - set - Shell Command Language - The Open Group Base Specifications Issue 7, 2018 edition <http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_25_03>
Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

autogen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+set -o errexit
+
 srcdir="$(dirname "$(readlink -f $0)")"
 
 (test -f $srcdir/configure.ac) || {

commit f7b9b8cbbb834fef46fa135dc02fa804c0bf0f73

Improve gtkdocize command detection in autogen script
Currently gtkdocize command detection relies on non shell built-in
`which` command and string test which is unnecessary and also broken
since commit adeb8ab.  This patch improves the detection by using the
built-in `command` utility.

Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

autogen.sh
@@ -61,8 +61,7 @@ srcdir="$(dirname "$(readlink -f "$0")")"
 
 
 echo "Running gtkdocize..."
-GTKDOCIZE=`which gtkdocize`
-if test -z $GTKDOCIZE; then
+if ! command -v gtkdocize >/dev/null; then
   echo "gtkdocize not found, skipping"
 else
   gtkdocize --srcdir "$srcdir" || exit 1